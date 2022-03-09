be62de1b597ba8c1348782996b624d82eaf92a09
ArthurWangCN <legendaryarthur@163.com>
2022-03-09 22:28:42 +0800

    引入gin

--- main.go
+++ main.go
@@ -5,12 +5,21 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/gin-gonic/gin"
 	"github.com/zserge/lorca"
 )
 
 func main() {
+	go func() {
+		gin.SetMode(gin.DebugMode)
+		router := gin.Default()
+		router.GET("/", func(c *gin.Context) {
+			c.Writer.Write([]byte("hello go"))
+		})
+		router.Run(":8080")
+	}()
 	var ui lorca.UI
-	ui, _ = lorca.New("https://baidu.com", "", 800, 600, "--disable-sync", "--disable-translate")
+	ui, _ = lorca.New("http://localhost:8080", "", 800, 600, "--disable-sync", "--disable-translate")
 	chSignal := make(chan os.Signal, 1)
 	signal.Notify(chSignal, syscall.SIGINT, syscall.SIGTERM)
 	select {
