a7082906ffb37a0fb57bfeca6d0a8a125dfa8da0
ArthurWangCN <legendaryarthur@163.com>
2022-03-14 21:50:47 +0800

    添加前端页面

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"embed"
+	"io/fs"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
@@ -9,17 +12,20 @@ import (
 	"github.com/zserge/lorca"
 )
 
+//go:embed frontend/dist/*
+var FS embed.FS
+
 func main() {
 	go func() {
 		gin.SetMode(gin.DebugMode)
 		router := gin.Default()
-		router.GET("/", func(c *gin.Context) {
-			c.Writer.Write([]byte("hello go"))
-		})
+		staticFiles, _ := fs.Sub(FS, "frontend/dist")
+		router.StaticFS("/static", http.FS(staticFiles))
 		router.Run(":8080")
 	}()
+
 	var ui lorca.UI
-	ui, _ = lorca.New("http://localhost:8080", "", 800, 600, "--disable-sync", "--disable-translate")
+	ui, _ = lorca.New("http://127.0.0.1:8080/static/index.html", "", 800, 600, "--disable-sync", "--disable-translate")
 	chSignal := make(chan os.Signal, 1)
 	signal.Notify(chSignal, syscall.SIGINT, syscall.SIGTERM)
 	select {
